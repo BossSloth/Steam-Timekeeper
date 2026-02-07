a8af2da834617e5b63c0f552c7872114319a395e
Boss Sloth <tddebart@gmail.com>
2026-02-07 10:57:12 +0100

    chore: fix deps

--- frontend/components/TimePopup.tsx
+++ frontend/components/TimePopup.tsx
@@ -1,11 +1,11 @@
 import { TimeManager } from '@components/TimeManager';
 import { routerHook } from '@steambrew/client';
 import React from 'react';
+import { container } from 'shared';
 import { EUIMode } from 'steam-types/Global/managers/PopupManager';
 import { SteamDialog } from 'SteamComponents';
 import { usePopupsStore } from './stores/popupsStore';
 import { Styles } from './Styles';
-import { container } from 'shared';
 
 export function TimePopup(): React.ReactNode {
   const { timePopup, setTimePopup } = usePopupsStore();
